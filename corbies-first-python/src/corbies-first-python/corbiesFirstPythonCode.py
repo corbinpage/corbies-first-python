36d88f6ab4d4dc77e0c350dbedf0b98c619ca57c
corbin.page <corbin.page@gmail.com>
2012-11-07 21:50:19 -0500

    Initial Commit

---
+++ corbies-first-python/src/corbies-first-python/corbiesFirstPythonCode.py
@@ -0,0 +1,18 @@
+'''
+Created on Nov 7, 2012
+
+@author: corbin.page
+'''
+
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y +a
+  
+print add(1,2)
+print addFixedValue(1) 
+print "Oh, and Hello World!" 
+print "And first Python commit" 
